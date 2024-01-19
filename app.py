e6dd383e985ffe3ec74e77a0321a098c521ba043
admin <admin@13-0538>
2024-01-19 15:36:00 +0800

    update

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 import gradio as gr
-import numpy as np
-import time
 
-def fake_diffusion(steps):
-    for i in range(steps):
-        time.sleep(1)
-        image = np.random.random((600, 600, 3))
-        yield image
-    image = np.ones((1000,1000,3), np.uint8)
-    image[:] = [255, 124, 0]
-    yield image
+def greet(name, intensity):
+    return "Hello " * intensity + name + "!"
 
-
-demo = gr.Interface(fake_diffusion, inputs=gr.Slider(1, 10, 3), outputs="image")
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
 
 demo.launch()
