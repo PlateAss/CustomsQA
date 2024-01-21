6a5bf342001ca770bb610b3a97c6ab7605f66087
PlateAss <10525232+PlateAss@user.noreply.gitee.com>
2024-01-21 15:55:15 +0800

    update

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import gradio as gr
-def modelscope_quickstart(name):
-    return "Welcome to modelscope, " + name + "!!"
-demo = gr.Interface(fn=modelscope_quickstart, inputs="text", outputs="text")
+import random
+import time
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("清除")
+
+    def respond(message, chat_history):
+        bot_message = random.choice(["你好吗？", "我爱你", "我很饿"])
+        chat_history.append((message, bot_message))
+        time.sleep(1)
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    clear.click(lambda: None, None, chatbot, queue=False)
+
 demo.launch()
