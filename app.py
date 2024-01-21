4c56f57662de3fcdde5dcda3aafa8dfe22f80029
PlateAss <10525232+PlateAss@user.noreply.gitee.com>
2024-01-21 14:29:00 +0800

    update

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import gradio as gr
-import os
-
-# 输入一张图片，旋转45°后输出
-def image_mod(image):
-    return image.rotate(45)
-
-
-demo = gr.Interface(image_mod, gr.Image(type="pil"), "image",
-    allow_flagging="never")
-
-if __name__ == "__main__":
-    demo.launch()
+def modelscope_quickstart(name):
+    return "Welcome to modelscope, " + name + "!!"
+demo = gr.Interface(fn=modelscope_quickstart, inputs="text", outputs="text")
+demo.launch()
