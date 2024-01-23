885b89580bfbd249b6a1d015d15b6ada632f5a62
admin <admin@13-0538>
2024-01-23 16:26:36 +0800

    update

--- pdf2xlsx.py
+++ pdf2xlsx.py
@@ -6,6 +6,7 @@ chs=[]
 chsheadtemp=""
 chstemp=""
 switch=0
+oldy0=0
 # for page in pdf.pages[29:273]:
 for page in pdf.pages[29:273]:
     for y in page.chars:
@@ -18,8 +19,13 @@ for page in pdf.pages[29:273]:
                 chstemp=''     
             elif (switch==2):
                 chs.append('') 
-                switch=0  
+                switch=0
+            elif switch==0 and abs(y["y0"]-oldy0)>15:
+                chshead.append(chsheadtemp)
+                chs.append('')
+                chsheadtemp=''  
             chsheadtemp+=y["text"]
+            oldy0=y["y0"]
             # print('head:'+y["text"]+":"+y["fontname"])
         elif (y["fontname"]=="XWAZUO+NotoSansSC-Light" or y['fontname']=='GMDXAW+NotoSansSC-Thin'):
             if (switch==0):
@@ -39,16 +45,25 @@ for page in pdf.pages[29:273]:
 chs.append(chstemp)
 del(chshead[0])
 del(chs[0])
-# print(len(chshead))
-# print(len(chs))
+print(len(chshead))
+print(len(chs))
 # for i in range(100):
 #     print(chshead[i]+":"+chs[i]) 
 
 # print(chshead[823]+":"+chs[823])
+
+charsplit = chs[6][56:57]
+charsplit2 = chs[7][18:19]
+print (charsplit)
 import pandas as pd
 table=[['question','answer']]
 for i in range(len(chshead)):
-    temp = [[chshead[i],chs[i]]]
+    if (chs[i]==''):
+        continue
+    chsnew = chs[i].replace(charsplit,'.')
+    chsnew = chsnew.replace(charsplit2,'')
+    # temp = [[chshead[i],chs[i]]]
+    temp = [[chshead[i],chsnew]]
     table.extend(temp)
 df = pd.DataFrame(table[1:],columns=table[0])
 df.to_excel('贸易便利化术语.xlsx',index=False)
