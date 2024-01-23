e2573216edd35c52d4beae8bad292c01bbb01b6a
admin <admin@13-0538>
2024-01-23 15:36:02 +0800

    update

--- pdf2xlsx.py
+++ pdf2xlsx.py
@@ -1,18 +1,54 @@
 import pdfplumber
+import chardet
 pdf = pdfplumber.open("UNECE 贸易便利化术语 TRADE Facilitation Terms v3 cn en ru.pdf")
-page = pdf.pages[29]
-print(page.chars)
-for i in page.chars:
-    print(i["fontname"],i["text"])
+chshead=[]
+chs=[]
+chsheadtemp=""
+chstemp=""
+switch=0
+# for page in pdf.pages[29:273]:
+for page in pdf.pages[29:273]:
+    for y in page.chars:
+        # print(y["text"]+":"+y["fontname"])
+        if (y["fontname"]=="SRKVRY+NotoSansSC-Bold"): 
+            if (switch==1):
+                # print(chstemp)
+                chs.append(chstemp)
+                switch=0
+                chstemp=''     
+            elif (switch==2):
+                chs.append('') 
+                switch=0  
+            chsheadtemp+=y["text"]
+            # print('head:'+y["text"]+":"+y["fontname"])
+        elif (y["fontname"]=="XWAZUO+NotoSansSC-Light" or y['fontname']=='GMDXAW+NotoSansSC-Thin'):
+            if (switch==0):
+                # print(chsheadtemp+":")
+                chshead.append(chsheadtemp) 
+                switch=1
+                chsheadtemp=''
+            elif (switch==2):
+                switch=1
+            chstemp+=y["text"]
+            # print('list:'+ y["text"]+":"+y["fontname"])
+        else:
+            if (switch==0):
+                chshead.append(chsheadtemp) 
+                switch=2
+                chsheadtemp=''
+chs.append(chstemp)
+del(chshead[0])
+del(chs[0])
+# print(len(chshead))
+# print(len(chs))
+# for i in range(100):
+#     print(chshead[i]+":"+chs[i]) 
 
-for i in page.lines:
-    print(i)
-print(page.width,page.height)
-page = page.crop((272,0,419,595))
-im = page.to_image(resolution=150)
-im.draw_rects(page.extract_words())
-im
-text = page.extract_text()
-print(text)
-text = page.extract_table(table_settings={"vertical_strategy":"text"})
-print(text)
+# print(chshead[823]+":"+chs[823])
+import pandas as pd
+table=[['question','answer']]
+for i in range(len(chshead)):
+    temp = [[chshead[i],chs[i]]]
+    table.extend(temp)
+df = pd.DataFrame(table[1:],columns=table[0])
+df.to_excel('贸易便利化术语.xlsx',index=False)
