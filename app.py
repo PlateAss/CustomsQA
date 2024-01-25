c7b991385e4a6ec0198cad4acb8f1b27319ae1a7
PlateAss <10525232+PlateAss@user.noreply.gitee.com>
2024-01-25 22:42:09 +0800

    update

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ if xlab==1:
 from dataclasses import asdict
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.utils import logging
-
 from tools.transformers.interface import GenerationConfig, generate_interactive
-
-logger = logging.get_logger(__name__)
-
 import gradio as gr
 
 def load_model():
@@ -22,9 +17,7 @@ def load_model():
     if xlab==1:
         model_name_or_path = "/home/xlab-app-center/internlm2-chat-7b"
     model = (
-        AutoModelForCausalLM.from_pretrained(model_name_or_path, trust_remote_code=True)
-        # .to(torch.bfloat16)
-        # .cuda()
+        AutoModelForCausalLM.from_pretrained(model_name_or_path, trust_remote_code=True).to(torch.bfloat16).cuda()
     )
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, trust_remote_code=True)
     return model, tokenizer
