a45f5b8276affd376fcd2c499ff219166fdf4de9
admin <admin@13-0538>
2024-01-30 13:38:40 +0800

    update

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],template
 qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectordb.as_retriever(),return_source_documents=True,chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
 
 import gradio as gr
-with gr.Blocks(title="通关问答") as demo:    
+with gr.Blocks(title="智贸辅助") as demo:    
     chatbot = gr.Chatbot(show_label=False)
     with gr.Row():
         with gr.Column(scale=5):
@@ -130,61 +130,3 @@ with gr.Blocks(title="通关问答") as demo:
     clear.click(lambda: None, None, chatbot, queue=False)
 demo.queue()
 demo.launch()
-
-
-
-
-    
-# from dataclasses import asdict
-# import torch
-# from transformers import AutoModelForCausalLM, AutoTokenizer
-# from tools.transformers.interface import GenerationConfig, generate_interactive
-# import gradio as gr
-
-# def load_model():
-#     model_name_or_path = modelpath[xlab]
-#     model = (AutoModelForCausalLM.from_pretrained(model_name_or_path, trust_remote_code=True)
-#     .to(torch.float16)
-#     .cuda())
-#     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, trust_remote_code=True)
-#     return model, tokenizer
-
-# def prepare_generation_config():
-#     generation_config = GenerationConfig(max_length=2048, top_p=0.8, temperature=0.7)
-#     return generation_config
-
-# user_prompt = "<|User|>:{user}\n"
-# robot_prompt = "<|Bot|>:{robot}<eoa>\n"
-# cur_query_prompt = "<|User|>:{user}<eoh>\n<|Bot|>:"
-
-
-# def combine_history(history):
-#     total_prompt = ""
-#     for msg in history:
-#         cur_prompt = user_prompt.replace("{user}", msg[0])
-#         total_prompt += cur_prompt
-#         cur_prompt = robot_prompt.replace("{robot}", msg[1])
-#         total_prompt += cur_prompt
-#     total_prompt += cur_query_prompt.replace("{user}", history[-1][0])
-#     return total_prompt
-
-# model, tokenizer = load_model()
-# generation_config = prepare_generation_config()
-# with gr.Blocks() as demo:
-#     chatbot = gr.Chatbot()
-#     msg = gr.Textbox()
-#     clear = gr.Button("清除")
-
-#     def user(user_message, history):
-#         return "", history + [[user_message, None]]
-
-#     def bot(history):
-#         history[-1][1] = ""
-#         for character in generate_interactive(model=model,tokenizer=tokenizer,prompt=combine_history(history),additional_eos_token_id=103028,**asdict(generation_config)):
-#             history[-1][1] = character
-#             yield history
-
-#     msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)
-#     clear.click(lambda: None, None, chatbot, queue=False)
-# demo.queue()
-# demo.launch()
